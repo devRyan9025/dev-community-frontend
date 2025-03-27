8ce7baff7837ba0040bf97bdbd8593b5431c08ab
devRyan9025 <alex7006@naver.com>
2025-03-27 15:41:30 +0900

    Feat: axios 전역 파일 생성

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: '/api',
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // ✅ 토큰 자동 첨부
+  }
+  return config;
+});
+
+export default instance;
