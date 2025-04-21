023512c9ea7e4261c0b36a23c296f62484e1bc64
devRyan9025 <alex7006@naver.com>
2025-04-21 18:00:13 +0900

    Feat : getFileHash 파일 생성

---
+++ src/util/getFileHash.js
@@ -0,0 +1,6 @@
+export default async function getFileHash(file) {
+  const buffer = await file.arrayBuffer();
+  const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
